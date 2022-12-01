0b51ac595fc6c1d78573ee28e014fdc7032448b9
xemesis <62959868+xemesis3@users.noreply.github.com>
2022-12-01 20:02:24 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,5 +4,5 @@ curl -fsSL https://get.docker.com/ | sh
 sudo systemctl start docker
 sudo systemctl status docker
 sudo systemctl enable docker
-docker pull chynr/deeznuts:latest
-docker run -itd --name webserver -p 8080:80 deeznuts:latest
+sudo docker pull chynr/deeznuts:latest
+sudo docker run -itd --name webserver -p 8080:80 deeznuts:latest
