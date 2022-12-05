93b512471275c774b780d16452763bde9701baad
xemesis <62959868+xemesis3@users.noreply.github.com>
2022-12-05 19:42:44 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/sudo bash
 sudo yum update -y
-sudo yum remove docker docker-common docker-selinux docker-engine
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce -y
+sudo yum install vim -y
+sudo yum install git -y
+sudo yum install docker -y
+sleep 5
 sudo systemctl start docker
-sudo systemctl status docker
 sudo systemctl enable docker
 sudo docker pull chynr/rally:latest
 sleep 3
