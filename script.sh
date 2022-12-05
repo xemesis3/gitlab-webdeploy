ce5fc17a66b104e31d783ab655149d77f84a31a4
xemesis <62959868+xemesis3@users.noreply.github.com>
2022-12-05 14:18:04 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/sudo bash
-yum update -y
-curl -fsSL https://get.docker.com/ | sh
+sudo yum update -y
+sudo yum remove docker docker-common docker-selinux docker-engine
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce -y
 sudo systemctl start docker
 sudo systemctl status docker
 sudo systemctl enable docker
